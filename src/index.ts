524162b7da15be0286a8cfeb43426997a1f7a27e
anibalmgr <anibal.gatica@mail.udp.cl>
2023-02-11 20:53:25 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+const auth = process.env.AUTH;
+
+app.get("/", (_req, res) => {
+  res.send("Hello, what are you doing here?");
+});
+
+app.post("/post", (req, res) => {
+  if (req.body.auth === auth) {
+    res.status(200).send(true);
+  } else {
+    res.status(400).send(false);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
