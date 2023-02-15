bb01860242f9e2b5ccb0f0277e58b27ea7fb1a8c
anibalmgr <anibal.gatica@mail.udp.cl>
2023-02-15 08:16:40 +0000

    deleting console log

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 const port = process.env.PORT;
 const auth = process.env.AUTH;
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.get("/", (_req, res) => {
-  res.send("Hello, what are you doing here?");
+  res.send("Hello, what are you doing there?");
 });
 
 app.post("/blog-auth", (req, res) => {
-  console.log(req.body);
-
   if (req.body?.pass === auth) {
     return res.status(200).send(true);
   } else {
